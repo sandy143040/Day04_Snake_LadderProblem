5332010856ec90ea1495bb89d96076b3fdb3998d
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-06-20 01:08:54 +0530

    [Sandeep] Add: check for option No play,Snake or Ladder

--- SnakeAndLadder/SnakeAndLadder/SnakeLadder.cs
+++ SnakeAndLadder/SnakeAndLadder/SnakeLadder.cs
@@ -19,6 +19,23 @@ namespace SnakeAndLadder
             Random random = new Random();
             int rollsdie = random.Next(1, 7);
             Console.WriteLine("Player gets a number after rolling die: "+rollsdie);
+
+            //selecting an option (No Play, Ladder, Snake)
+            int option = random.Next(0, 3);
+            switch(option)
+            {
+                case 0:
+                    Console.WriteLine("No Play!");
+                    break;
+                case 1:
+                    Console.WriteLine("You got a ladder: "+rollsdie+" Number of position player moved ahead");
+                    break;
+                case 2:
+                    Console.WriteLine("Oops! You encountered a snake: " + rollsdie +" Number of position player moved behind ");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
