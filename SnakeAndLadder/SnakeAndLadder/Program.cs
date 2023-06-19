98d02846a297e964b3bf5ce3a00918bee6dc9425
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-06-20 00:00:45 +0530

    [Sandeep] Add:Player at start position

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            //welcome message
+            Console.WriteLine("Welcome to Snake & Ladder problem");
+            
+            SnakeLadder snakeLadder = new SnakeLadder();
+            snakeLadder.StartPosition();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SnakeAndLadder/SnakeAndLadder/SnakeLadder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class SnakeLadder
+    {
+        public void StartPosition()
+        {
+            int startPosition = 0;
+            Console.WriteLine("Game played with single player at start position: "+ startPosition);
+        }
+    }
+}
